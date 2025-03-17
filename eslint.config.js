c79a24c9bca982f3f08449cee1d7d3db9d505f95
Michał Kołacz <michal.kolacz45@gmail.com>
2025-03-17 20:51:23 +0100

    fix: correct formatting of eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,7 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
-import pluginStorybook from 'eslint-plugin-storybook'
+import pluginStorybook from "eslint-plugin-storybook";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -16,7 +16,7 @@ export default [
   pluginReact.configs.flat.recommended,
   pluginReact.configs.flat["jsx-runtime"],
   {
-    files: ['**/*.stories.@(ts|tsx|js|jsx|mjs|cjs)'],
+    files: ["**/*.stories.@(ts|tsx|js|jsx|mjs|cjs)"],
     plugins: {
       pluginStorybook,
     },
