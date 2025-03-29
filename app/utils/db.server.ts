acbf2620848f95f46e77acf4c69d747d048e5ed0
Michał Kołacz <michal.kolacz45@gmail.com>
2025-03-29 17:30:50 +0100

    refactor: enhance Prisma logging with Sentry error tracking and improve query event handling

--- app/utils/db.server.ts
+++ app/utils/db.server.ts
@@ -1,5 +1,6 @@
 import { remember } from "@epic-web/remember";
 import { PrismaClient } from "@prisma/client/index.js";
+import * as Sentry from "@sentry/node";
 
 import { styleText } from "node:util";
 
@@ -9,24 +10,35 @@ export const prisma = remember("prisma", () => {
   const client = new PrismaClient({
     log: [
       { level: "query", emit: "event" },
-      { level: "error", emit: "stdout" },
-      { level: "warn", emit: "stdout" },
+      { level: "error", emit: "event" },
+      { level: "warn", emit: "event" },
+      { level: "info", emit: "stdout" },
     ],
   });
-  client.$on("query", async (e) => {
-    if (e.duration < logThreshold) return;
+  client.$on("query", async (event) => {
+    if (event.duration < logThreshold) return;
     const color =
-      e.duration < logThreshold * 1.1
+      event.duration < logThreshold * 1.1
         ? "green"
-        : e.duration < logThreshold * 1.2
+        : event.duration < logThreshold * 1.2
         ? "blue"
-        : e.duration < logThreshold * 1.3
+        : event.duration < logThreshold * 1.3
         ? "yellow"
-        : e.duration < logThreshold * 1.4
+        : event.duration < logThreshold * 1.4
         ? "redBright"
         : "red";
-    const dur = styleText(color, `${e.duration}ms`);
-    console.info(`prisma:query - ${dur} - ${e.query}`);
+    const dur = styleText(color, `${event.duration}ms`);
+    console.info(`prisma:query - ${dur} - ${event.query}`);
+  });
+
+  client.$on("error", (event) => {
+    const prismaError = new Error(event.message);
+    Sentry.captureException(prismaError, {
+      extra: {
+        ...event,
+      },
+    });
+    console.error("🛑", "Prisma error", event.message, event.target);
   });
   void client.$connect();
   return client;
