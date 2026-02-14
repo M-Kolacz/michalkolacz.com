9e8f50d1334afaa404598d31d397905e6c355dce
Michał Kołacz <michal.kolacz45@gmail.com>
2026-02-14 11:02:36 +0100

    feat: specify testMatch for visual regression tests

--- playwright-visual.config.ts
+++ playwright-visual.config.ts
@@ -4,6 +4,7 @@ const BASE_URL = process.env.TEST_BASE_URL || 'http://localhost:6006'
 
 export default defineConfig({
 	testDir: './tests',
+	testMatch: 'visual.spec.ts',
 	outputDir: './tests/__failed-snapshots__',
 	fullyParallel: true,
 	snapshotPathTemplate: '{testDir}/__screenshots__/{arg}{ext}',
