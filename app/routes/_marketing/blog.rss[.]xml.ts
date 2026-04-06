05f624dd74c2ce3af033fcb48369f7ad7767b9cd
Michał Kołacz <michal.kolacz45@gmail.com>
2026-04-06 17:00:36 +0200

    feat(blog): add RSS feed and Open Graph meta tags (#45)
    Add RSS 2.0 feed at /blog/rss.xml with all published posts and proper
    XML escaping. Add OG meta tags (title, description, type, image) to
    individual post pages and generic OG tags to the listing page.

---
+++ app/routes/_marketing/blog.rss[.]xml.ts
@@ -0,0 +1,48 @@
+import { getCachedPostListings } from '#app/utils/blog/cache.server.ts'
+import { getDomainUrl } from '#app/utils/misc.tsx'
+import { type Route } from './+types/blog.rss[.]xml.ts'
+
+export async function loader({ request }: Route.LoaderArgs) {
+	const posts = await getCachedPostListings()
+	const domainUrl = getDomainUrl(request)
+	const blogUrl = `${domainUrl}/blog`
+	const feedUrl = `${domainUrl}/blog/rss.xml`
+
+	const rss = `<?xml version="1.0" encoding="UTF-8"?>
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+  <channel>
+    <title>Michal Kolacz Blog</title>
+    <description>Blog posts by Michal Kolacz</description>
+    <link>${blogUrl}</link>
+    <atom:link href="${feedUrl}" rel="self" type="application/rss+xml"/>
+    <language>en</language>${posts
+			.map(
+				(post) => `
+    <item>
+      <title>${escapeXml(post.title)}</title>
+      <description>${escapeXml(post.description)}</description>
+      <pubDate>${new Date(post.date).toUTCString()}</pubDate>
+      <link>${domainUrl}/blog/${post.slug}</link>
+      <guid isPermaLink="true">${domainUrl}/blog/${post.slug}</guid>
+    </item>`,
+			)
+			.join('')}
+  </channel>
+</rss>`
+
+	return new Response(rss, {
+		headers: {
+			'Content-Type': 'application/rss+xml; charset=utf-8',
+			'Cache-Control': `public, max-age=${60 * 5}`,
+		},
+	})
+}
+
+function escapeXml(str: string): string {
+	return str
+		.replace(/&/g, '&amp;')
+		.replace(/</g, '&lt;')
+		.replace(/>/g, '&gt;')
+		.replace(/"/g, '&quot;')
+		.replace(/'/g, '&apos;')
+}
