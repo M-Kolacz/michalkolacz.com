5065fc304f4eb388b28e71d33a1805603e8b9963
Michał Kołacz <michal.kolacz45@gmail.com>
2026-04-09 22:14:37 +0200

    fix: correct database url for prisma

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'prisma/config'
+import 'dotenv/config'
+import { defineConfig, env } from 'prisma/config'
 
 export default defineConfig({
 	migrations: {
 		seed: 'tsx prisma/seed.ts',
+	},
+	datasource: {
+		url: env('DATABASE_URL'),
 	},
 })
