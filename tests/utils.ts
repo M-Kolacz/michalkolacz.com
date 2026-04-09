253c6a81f846b627f544f11b7627e22e6ad761c6
Michał Kołacz <michal.kolacz45@gmail.com>
2026-04-09 18:00:47 +0200

    chore: migrate from epic config

--- tests/utils.ts
+++ tests/utils.ts
@@ -13,7 +13,7 @@ export const { t } = initI18n({
 export const BASE_URL = 'https://www.epicstack.dev'
 
 export function convertSetCookieToCookie(setCookie: string) {
-	const parsedCookie = setCookieParser.parseString(setCookie)
+	const parsedCookie = setCookieParser.parseString(setCookie)!
 	return new URLSearchParams({
 		[parsedCookie.name]: parsedCookie.value,
 	}).toString()
