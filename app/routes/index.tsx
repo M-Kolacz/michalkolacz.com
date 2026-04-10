34533c8a105c52cdc1459df3b3e21e1b074e4899
Michał Kołacz <michal.kolacz45@gmail.com>
2026-04-10 15:40:52 +0200

    chore: correct text

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -10,7 +10,7 @@ export default function Index() {
 		<main className="flex w-full flex-col items-center p-8 lg:py-16">
 			<div className="flex w-full flex-col gap-4 md:flex-row md:items-center lg:w-auto lg:gap-7">
 				<div className="flex shrink-0 flex-col gap-4 text-black dark:text-white">
-					<h2 className="text-h2">{t('homepage.hero.heading')}</h2>
+					<h2 className="text-h2">{t('homepage.hero.heading')}.</h2>
 					<p className="text-p1">{t('homepage.hero.subheading')}</p>
 				</div>
 				<img
